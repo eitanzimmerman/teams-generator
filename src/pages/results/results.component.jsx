0f4fcdcb818b97ccf11a191ec681bdb1f803850a
eitanzimmerman <eitanzimmerman@gmail.comgit config --global user.email eitanzimmerman@gmail.comgit config --global user.email eitanzimmerman@gmail.com>
2020-11-03 00:25:37 +0200

    added icons to results page and goback functionality

--- src/pages/results/results.component.jsx
+++ src/pages/results/results.component.jsx
@@ -1,5 +1,6 @@
 
 import React, {useState, useEffect} from 'react';
+import { withRouter } from 'react-router-dom'
 import './results.styles.scss';
 
 import useCopyToClipBoard from '../../hooks/useCopyToClipboard';
@@ -7,10 +8,15 @@ import { handleArrayCopy } from '../../utils/handle-array-copy';
 
 import TeamCard from '../../components/team-card/team-card.component';
 import { ReactComponent as CopyIcon} from '../../assets/content_copy.svg';
+import { ReactComponent as SaveIcon} from '../../assets/floppy-disk.svg';
+import { ReactComponent as ReturnIcon} from '../../assets/undo.svg';
+
+import CostumButton from '../../components/costum-button/costum-button.component';
 
 
-const Results = ({location}) => {
+const Results = ({location, history}) => {
     const {teams, title, players} = location.state
+
     const [isCopied, handleCopy] = useCopyToClipBoard(3000);
     return (
         <div className='results-page'>
@@ -23,11 +29,23 @@ const Results = ({location}) => {
                    teams.map((team, index) => <TeamCard key={index} team={team}/>)
                }
             </div>
-            <button  style ={{'backgroundColor' : isCopied ? '#e0fbfc' : ''}} className='copy-container' onClick={()=> handleCopy(handleArrayCopy(teams))}>
-               {isCopied ? <div className='copy-success'><span className='copy-success-text'>Copied</span></div> : <CopyIcon className='copy-icon'/>}
-            </button>
+            <div className='results-page-footer'>
+                <div className='icons-container'>
+                    <button  style ={{'backgroundColor' : isCopied ? '#e0fbfc' : ''}} className='icon-container' onClick={()=> handleCopy(handleArrayCopy(teams))}>
+                    {isCopied ? <div className='copy-success'><span className='copy-success-text'>Copied</span></div> : <CopyIcon className='result-icon'/>}
+                    </button>
+                    <button className='icon-container'>
+                        <SaveIcon className='result-icon'/>
+                    </button>
+                    <button className='icon-container' onClick={() => history.goBack()}>
+                        <ReturnIcon className='result-icon'/>
+                    </button>
+                </div>
+                <CostumButton size='big' >תן עוד ערבוב</CostumButton>
+            </div>
+            
         </div>
     )
 }
 
-export default Results;
+export default withRouter(Results);
